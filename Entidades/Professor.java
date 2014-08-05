11c67b74c9da94d7ea029763a1a4829ae07cbf7e
Augusto Felix Pereira Neto <augusto.felix@dce.ufpb.br>
2014-08-05 12:17:00 -0300

    Entidades e testes

---
+++ Entidades/Professor.java
@@ -0,0 +1,18 @@
+package Entidades;
+
+public class Professor extends Pessoa{
+	
+	private String titulo;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+	
+
+}
